0c8e21197d88c35cffac41e138bf79cd23f27f98
Eman-sh <amoooony@hotmail.com>
2019-06-26 22:50:31 +0300

    Dom Hw

--- HW_part2.js
+++ HW_part2.js
@@ -1,5 +1,11 @@
 
+
+document.querySelector('button#randomize').addEventListener("click", randomImage);
+
+function randomImage() {
+
 const newImg = document.createElement("img");
 newImg.setAttribute("src", "https://source.unsplash.com/random");
 document.body.appendChild(newImg);
 newImg=document.createElement("div.profilePicture");
+};

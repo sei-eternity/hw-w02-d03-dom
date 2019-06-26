a553fd97a0e163bd0db52d2bec2f3684f80cb30a
imunirah <moneerah.md@gmail.com>
2019-06-26 16:03:55 +0300

    dom

---
+++ code/app.js
@@ -0,0 +1,4 @@
+const newImg = document.createElement('img');
+newImg.setAttribute('src', 'https://source.unsplash.com/1600x900');
+document.body.appendChild(newImg);
+const eventType = 'click'; 
---
+++ code/more-dom-manipulation/JavaScript.js
@@ -0,0 +1,61 @@
+
+
+
+//Create a new img element and set its src attribute to a picture of you. Put that new img element in the div with the class of "profilePicture"
+const image = document.querySelector('.profilePicture');
+console.log(image);
+
+//Change the font-family of the page to "Arial, sans-serif"
+const content = document.querySelector('body');
+content.style.fontFamily ='Arial,sans-serif';
+
+//Change each li's text color to "rebeccapurple"
+const listitem = document.querySelector('li');
+allListitem.style.color = ('#rebeccapurple');
+console.log(li);
+
+//Give each li the class of "listitem"
+const list = document.querySelector('.listitem');
+console.log(list);
+
+//Replace each of the span tags (nickname, favorite, hometown) with your own information (e.g. put your nickname in the nickname span)
+const spanTags = document.querySelectorAll('span');
+spanTag [0].innerText = 'Munirah Almadi';
+spanTag [1].innerText = 'I do not like animals';
+spanTag [2].innerText = 'Saudi Arabia / Riyadh';
+
+
+
+
+var books = [
+    {
+      title: "The Design of Everyday Things",
+      author: "Don Norman",
+      alreadyRead: false
+    },
+    {
+      title: "The Most Human Human",
+      author: "Brian Christian",
+      alreadyRead: true
+    },
+    {
+      title: "In Search of Lost Time",
+      author: "Marcel Proust",
+      alreadyRead: true
+    },
+    {
+      title: "Ulysses",
+      author: "James Joyce",
+      alreadyRead: true
+    },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      alreadyRead: true
+    }
+  ];
+
+  
+  //
+  const divTag = document.querySelector('.favoriteBooks');
+  divTag.appendChild(heading);
